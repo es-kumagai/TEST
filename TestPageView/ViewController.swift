faa6c96bc54a42c9df7871184401d68dad32f2d2
Tomohiro Kumagai <tomo@ez-net.jp>
2024-07-17 11:12:09 +0900

    Initial Commit

---
+++ TestPageView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  
+//  
+//  Created by Tomohiro Kumagai on 2024/07/17
+//  
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
